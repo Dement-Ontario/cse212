49a4a4fccbe6cbeb3c6f9fd0785ce5132a131ac7
Joseph T <jdtoronto999@outlook.com>
2026-01-09 16:02:58 -0800

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 public static class Arrays
 {
     /// <summary>
@@ -8,12 +12,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Problem 1
+        // 1. Make a result array that's as long as length
+        // 2. Make a for loop where i counts upwards from 1 and stops when i = length
+        // 3. Every loop, add number * i to result[i - 1]
+        // 4. Return result
 
-        return []; // replace this return statement with your own
+        var result = new double[length];
+
+        for (int i = 1; i <= length; ++i)
+        {
+            result[i - 1] = number * i;
+        }
+        
+        Debug.WriteLine(string.Join(", ", result));
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +38,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Problem 2
+        // 1. Use GetRange to copy the last items in the list based on the amount
+        //    and put them in their own "last items" array
+        // 2. Use RemoveRange to remove the items copied from the original list
+        // 3. Use InsertRange to insert the items from the
+        //    "last items" array into the start of the list
+
+        var lastItems = data.GetRange(data.Count - amount, amount).ToArray();
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0, lastItems);
+
+        Debug.WriteLine(string.Join(", ", lastItems));
+        Debug.WriteLine(string.Join(", ", data));
     }
 }
