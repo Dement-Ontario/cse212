0788287f4ffb8d0fe10fb623ee55bd09828d5ea0
Joseph T <jdtoronto999@outlook.com>
2026-01-16 17:15:53 -0800

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,60 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Make a queue and enqueue these values and priorities: (A, 1), (B, 2), (C, 3),
+    // then dequeue once
+    // Expected Result: PriorityQueue is [A (Pri:1), B (Pri:2), C (Pri:3)], then C is dequeued
+    // Defect(s) Found: The for loop in Dequeue didn't run for the last item in the queue
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        
+        priorityQueue.Enqueue("A", 1);
+        priorityQueue.Enqueue("B", 2);
+        priorityQueue.Enqueue("C", 3);
+
+        Assert.AreEqual("[A (Pri:1), B (Pri:2), C (Pri:3)]", priorityQueue.ToString());
+        Assert.AreEqual("C", priorityQueue.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Make a queue and enqueue these values and priorities: (A, 3), (B, 1), (C, 3),
+    // then dequeue once
+    // Expected Result: The code should dequeue A
+    // Defect(s) Found: The Dequeue function checked for whether the current index's priority was
+    // also equal to the high priority index's priority, so the high priority index was changed to
+    // the last high-priority index instead of the first
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("A", 3);
+        priorityQueue.Enqueue("B", 1);
+        priorityQueue.Enqueue("C", 3);
+
+        Assert.AreEqual("[A (Pri:3), B (Pri:1), C (Pri:3)]", priorityQueue.ToString());
+        Assert.AreEqual("A", priorityQueue.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Make a queue and enqueue these values and priorities: (A, 1), (B, 2), (C, 3),
+    // then dequeue until there's nothing left, then dequeue again
+    // Expected Result: InvalidOperationException error
+    // Defect(s) Found: Dequeue didn't remove anything from the queue
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("A", 1);
+        priorityQueue.Enqueue("B", 2);
+        priorityQueue.Enqueue("C", 3);
+
+        Assert.AreEqual("[A (Pri:1), B (Pri:2), C (Pri:3)]", priorityQueue.ToString());
+
+        priorityQueue.Dequeue();
+        priorityQueue.Dequeue();
+        priorityQueue.Dequeue();
+
+        Assert.ThrowsException<InvalidOperationException>(() => priorityQueue.Dequeue());
+    }
 }
